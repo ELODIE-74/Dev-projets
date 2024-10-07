9151e70fddee22b0ae7d1bd5b2dfb2d83a94c95b
Elodie Jance <elodie.jance@gmail.com>
2024-10-07 16:00:46 +0200

    ajout menu hamburger

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const hamburger = document.getElementById("hamburger");
+const menu = document.getElementById("menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active"); // Ajoute/enlève la classe active
+  menu.classList.toggle("active"); // Affiche/masque le menu
+});
